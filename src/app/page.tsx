5523bc87f2d5f7a10866f8db163c38b1c7cad16b
Patricia Souza <patricia.sz.br@gmail.com>
2025-11-03 23:30:47 -0300

    Logic Period

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
 import { PeriodSection } from '../components/period-section';
+import { Appointment as AppointmentPrisma } from '../generated/prisma/client';
+import {
+  Appointment,
+  AppointmentPeriod,
+  AppointmentPeriodDay,
+} from '../types/appointments';
 
 const appointments = [
   {
@@ -35,20 +41,73 @@ const appointments = [
   },
 ];
 
+const getPeriod = (hour: number): AppointmentPeriodDay => {
+  if (hour >= 9 && hour < 12) return 'morning';
+  if (hour >= 13 && hour < 18) return 'afternoon';
+  return 'evening';
+};
+
+function groupAppointmentByPeriod(
+  appointments: AppointmentPrisma[]
+): AppointmentPeriod[] {
+  const transformedAppointments: Appointment[] = appointments?.map((apt) => ({
+    ...apt,
+    time: apt.scheduleAt.toLocaleTimeString('pt-BR', {
+      hour: '2-digit',
+      minute: '2-digit',
+    }),
+    service: apt.description,
+    period: getPeriod(apt.scheduleAt.getHours()),
+  }));
+
+  const morningAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'morning'
+  );
+  const afternoonAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'afternoon'
+  );
+  const eveningAppointments = transformedAppointments.filter(
+    (apt) => apt.period === 'evening'
+  );
+
+  return [
+    {
+      title: 'Manhã',
+      type: 'morning',
+      timeRange: '09h-12h',
+      appointments: morningAppointments,
+    },
+    {
+      title: 'Tarde',
+      type: 'afternoon',
+      timeRange: '13h-18h',
+      appointments: afternoonAppointments,
+    },
+    {
+      title: 'Noite',
+      type: 'evening',
+      timeRange: '19h-21h',
+      appointments: eveningAppointments,
+    },
+  ];
+}
+
 export default function Home() {
+  const periods = groupAppointmentByPeriod(appointments);
+
   return (
     <div className="bg-background-primary p-6">
       <div className="flex items-center justify-between md:m-8">
-        <div>
-          <h1 className="text-title-size text-content-primary mb-2">
-            Sua Agenda
-          </h1>
-          <p className="text-paragraph-medium-size text-content-secondary">
-            Aqui você pode ver todos os clientes e serviços agendados para hoje.
-          </p>
-        </div>
+        <p>
+          {' '}
+          Aqui você pode ver todos os clientes e serviços agendados para hoje.
+        </p>
       </div>
-      <PeriodSection period={[]} />
+      <div className="pb-24 md:pb-0">
+        {periods.map((period, index) => (
+          <PeriodSection period={period} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/period-section/period-section.tsx
+++ src/components/period-section/period-section.tsx
@@ -1,23 +1,29 @@
-import { Cloud, Moon, Sun } from 'lucide-react';
+import { AppointmentPeriod } from '@/src/types/appointments';
+import { Cloudy, Moon, Sun } from 'lucide-react';
+
 type PeriodSectionProps = {
-  period: any;
+  period: AppointmentPeriod;
 };
+
 const periodIcons = {
   morning: <Sun className="text-accent-blue" />,
-  afternoon: <Cloud className="text-accent-orange" />,
-  everning: <Moon className="text-accent-yellow" />,
+  afternoon: <Cloudy className="text-accent-orange" />,
+  evening: <Moon className="text-accent-yellow" />,
 };
 
 export const PeriodSection = ({ period }: PeriodSectionProps) => {
   return (
     <section className="mb-8 bg-background-tertiary rounded-xl">
       <div className="flex items-center px-5 py-3 justify-between border-b border-[#2E2C30]">
-        <div>
-          {periodIcons[period.type]}
-          <h2 className="text-label-large-size to-content-primary ">
-            {period.title}
+        <div className="flex items-center gap-2">
+          {periodIcons[period?.type]}
+          <h2 className="text-label-large-size text-content-primary">
+            {period?.title}
           </h2>
         </div>
+        <span className="text-label-large-size text-content-secondary">
+          {period.timeRange}
+        </span>
       </div>
     </section>
   );
